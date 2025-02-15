148dc00209b85bb1171bda427d726742a347d92b
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-15 20:06:12 -0300

    Defining app permissions.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,8 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'ADMIN' })
+const ability = defineAbilityFor({ role: 'ADMIN', id: 'user_id' })
+
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteSomeoneElse = ability.can('delete', 'User')
