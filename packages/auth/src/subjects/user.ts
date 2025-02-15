9fd67ea0e54ca8f4abb9fb389eff694a83eb42a8
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-15 03:41:11 -0300

    Optimizing types in the auth package's subjects and modules using zod.

--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -1,1 +1,14 @@
+import { z } from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('invite'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('User'),
+])
+
 export type UserSubject = ['create' | 'delete' | 'manage' | 'invite', 'User']
