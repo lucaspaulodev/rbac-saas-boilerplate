7c0804170df278ce2035b45a1b36cba302c767da
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-18 00:39:23 -0300

    Create account route.

--- apps/backend/src/http/routes/auth/create-account.ts
+++ apps/backend/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,28 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User Created'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSomeEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithSomeEmail) {
+        return reply.status(400).send({ message: `E-mail is already in use` })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
