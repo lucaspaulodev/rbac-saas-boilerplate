f36c27b9e7be117318ec05fea27b588941a3b6b0
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-21 00:38:46 -0300

    Creating getUserMembership method in auth middleware.

--- apps/backend/src/http/middlewares/auth.ts
+++ apps/backend/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnauthorizedError } from '../routes/__errors/unauthorized-error'
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
@@ -13,6 +15,25 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
       } catch (error) {
         throw new UnauthorizedError('Invalid auth token')
       }
+    }
+
+    request.getUserMembership = async (slug: string) => {
+      const userId = await request.getCurrentUserId()
+
+      const member = await prisma.member.findFirst({
+        where: { organization: { slug }, userId },
+        include: {
+          organization: true,
+        },
+      })
+
+      if (!member) {
+        throw new UnauthorizedError('User is not a member of this organization')
+      }
+
+      const { organization, ...membership } = member
+
+      return { organization, membership }
     }
   })
 })
