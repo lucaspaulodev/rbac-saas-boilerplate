bc1544d8d6e3128838f7af18422829f4560fc6a9
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-23 18:16:37 -0300

    Creating create-project route.

---
+++ apps/backend/src/http/routes/projects/create-project.ts
@@ -0,0 +1,67 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { generateSlug } from '@/utils/generate-slug'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../__errors/unauthorized-error'
+
+export async function createProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/projects',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Create a new project',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            description: z.string(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.object({
+              projectId: z.string().uuid(),
+            }),
+            400: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('create', 'Project')) {
+          throw new UnauthorizedError('You are not allowed to create a project')
+        }
+
+        const { name, description } = request.body
+
+        const { id: projectId } = await prisma.project.create({
+          data: {
+            name,
+            description,
+            organizationId: organization.id,
+            slug: generateSlug(name),
+            ownerId: userId,
+          },
+        })
+
+        return reply.status(201).send({ projectId })
+      },
+    )
+}
