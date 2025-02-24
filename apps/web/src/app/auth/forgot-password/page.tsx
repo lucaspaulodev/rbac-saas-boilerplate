b7d2cfef77d21dfb9ceee7bb9e99d496bd090c4e
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-24 19:33:06 -0300

    Creating Sign-in, Sign-up, and forgot-password pages.

---
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export default function ForgotPasswordPage() {
+  return (
+    <form action="">
+      <div className="space-y-4">
+        <div className="space-y-1">
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" type="email" id="email" />
+        </div>
+
+        <Button type="submit" className="w-full">
+          Recover Password
+        </Button>
+
+        <Button variant="link" className="w-full" size="sm" asChild>
+          <Link href="/auth/sign-in">Back to Sign In</Link>
+        </Button>
+      </div>
+    </form>
+  )
+}
--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,10 +7,14 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="">
       <div className="space-y-4">
+        <div className="space-y-1">
+          <Label htmlFor="name">Name</Label>
+          <Input name="name" type="name" id="name" />
+        </div>
         <div className="space-y-1">
           <Label htmlFor="email">Email</Label>
           <Input name="email" type="email" id="email" />
@@ -19,17 +23,16 @@ export default function SignInPage() {
           <Label htmlFor="password">Password</Label>
           <Input name="password" type="password" id="password" />
         </div>
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot Password?
-        </Link>
+        <div className="space-y-1">
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input name="confirmPassword" type="password" id="confirmPassword" />
+        </div>
+
         <Button type="submit" className="w-full">
-          Sign In
+          Create Account
         </Button>
         <Button variant="link" className="w-full" size="sm" asChild>
-          <Link href="/auth/sign-up">Don&apos;t have an account? Sign Up</Link>
+          <Link href="/auth/sign-in">Already have an account? Sign In</Link>
         </Button>
         <Separator />
         <Button variant="outline" className="w-full">
@@ -38,7 +41,7 @@ export default function SignInPage() {
             alt="Github logo"
             className="mr-2 size-4 dark:invert"
           />
-          Sign In with Github
+          Sign Up with Github
         </Button>
       </div>
     </form>
