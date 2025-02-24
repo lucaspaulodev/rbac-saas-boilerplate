ef0765670e847a9d52aadccded782015e0320b09
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-24 17:47:09 -0300

    Intalling and creating web project with NextJS.

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Saas with role based access control boilerplate',
+  description: 'Saas with role based access control boilerplate',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  )
+}
