39a85d3e9464525c02a003d15615ac998829815a
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-26 20:38:03 -0300

    Creating signInWithEmailAndPassword action.

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,15 +1,51 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
+const signInSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+})
+
 export async function signInWithEmailAndPassword(formData: FormData) {
-  const email = formData.get('email')
-  const password = formData.get('password')
+  const result = signInSchema.safeParse(Object.fromEntries(formData))
 
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return { success: false, message: null, errors }
+  }
 
-  console.log(result)
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+
+    // Development only. Remove console.log before deploying
+    console.log(token)
+
+    return { success: true, message: null, errors: null }
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    return {
+      success: false,
+      message: 'An unexpected error occurred',
+      errors: null,
+    }
+  }
 }
