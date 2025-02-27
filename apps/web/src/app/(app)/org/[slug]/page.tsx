7fe5ea18de7e8db92787d894e50035b2cfc28114
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-27 20:26:17 -0300

    Update Organization Switcher to display current organization by slug stored in cookies.

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/header'
+
+async function Projects() {
+  return (
+    <div className="py-4">
+      <Header />
+    </div>
+  )
+}
+
+export default Projects
--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,4 +1,5 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import { cookies } from 'next/headers'
 import Link from 'next/link'
 
 import { getOrganizations } from '@/http/get-organizations'
@@ -15,12 +16,34 @@ import {
 } from './ui/dropdown-menu'
 
 export async function OrganizationSwitcher() {
+  const cookieStore = await cookies()
+  const currentOrg = cookieStore.get('org')?.value
   const { organizations } = await getOrganizations()
+
+  const currentOrganization = organizations.find(
+    (organization) => organization.slug === currentOrg,
+  )
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex w-[168px] items-center gap-2 rounded p-1 text-sm outline-none focus-visible:ring-2 focus-visible:ring-primary">
-        <span className="text-muted-foreground">Select Organization</span>
+        {currentOrganization ? (
+          <>
+            <Avatar className="mr-2 size-4">
+              {currentOrganization.avatarUrl && (
+                <AvatarImage src={currentOrganization.avatarUrl} />
+              )}
+              <AvatarFallback>
+                {currentOrganization.name.slice(0, 2)}
+              </AvatarFallback>
+            </Avatar>
+            <span className="truncate text-left">
+              {currentOrganization.name}
+            </span>
+          </>
+        ) : (
+          <span className="text-muted-foreground">Select Organization</span>
+        )}
         <ChevronsUpDown className="ml-auto size-4 text-muted-foreground" />
       </DropdownMenuTrigger>
       <DropdownMenuContent
@@ -33,7 +56,7 @@ export async function OrganizationSwitcher() {
           <DropdownMenuLabel>Organizations</DropdownMenuLabel>
           {organizations.map((organization) => (
             <DropdownMenuItem key={organization.id} asChild>
-              <Link href={`/organizations/${organization.slug}`}>
+              <Link href={`/org/${organization.slug}`}>
                 <Avatar className="mr-2 size-4">
                   {organization.avatarUrl && (
                     <AvatarImage src={organization.avatarUrl} />
