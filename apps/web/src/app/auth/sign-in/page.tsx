6fba387866dcf89ba27081566294a837c30004e3
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-26 22:52:07 -0300

    Styles updates.

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { SignInForm } from './sign-in-form'
 
 export default function SignInPage() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <SignInForm />
     </div>
   )
