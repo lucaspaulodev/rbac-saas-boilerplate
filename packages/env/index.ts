c015e2073a15c94c3d377b146299c1d9cd7f02be
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-28 01:30:30 -0300

    Setting NEXT_PUBLIC_API_URL as shared variable in index.ts from env package.

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -11,7 +11,11 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_SECRET: z.string(),
     GITHUB_OAUTH_REDIRECT_URI: z.string().url(),
   },
+  shared: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
   runtimeEnv: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     SERVER_PORT: process.env.SERVER_PORT,
     JWT_SECRET: process.env.JWT_SECRET,
