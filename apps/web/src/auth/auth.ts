791851f7b8a4328df40a5789f27ce4d704a7cc3a
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-28 00:38:46 -0300

    Implementing in auth.ts file these helpers: getCurrentOrg, getCurrentMembership and ability.

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,8 +1,9 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
-
 export async function isAuthenticated() {
   const cookieStore = await cookies()
   const token = cookieStore.get('token')
@@ -25,4 +26,37 @@ export async function auth() {
   } catch (error) {}
 
   redirect('/api/auth/sign-out')
+}
+
+export async function getCurrentOrg() {
+  const cookieStore = await cookies()
+
+  return cookieStore.get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const currentOrg = await getCurrentOrg()
+
+  if (!currentOrg) {
+    return null
+  }
+
+  const { membership } = await getMembership(currentOrg)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
