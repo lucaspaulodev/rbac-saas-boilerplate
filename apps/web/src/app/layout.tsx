bd021d70ffdab02a972d0814000b316b93db4b59
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-28 01:05:13 -0300

    Importing ThemeProvider in our root layout.tsx file.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-
+import { ThemeProvider } from 'next-themes'
 export const metadata: Metadata = {
   title: 'Saas with role based access control boilerplate',
   description: 'Saas with role based access control boilerplate',
@@ -13,8 +13,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`dark antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`antialiased`}>
+        <ThemeProvider
+          attribute={'class'}
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
