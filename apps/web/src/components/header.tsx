512d879f36a71e61f92efd5efb6c343436eac979
Lucas Paulo <lucaspaulodev@gmail.com>
2025-02-28 00:41:29 -0300

    Displaying 'Projects' in Header to authorized users.

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,15 +1,18 @@
 import { Slash } from 'lucide-react'
 
+import { ability } from '@/auth/auth'
 import { OrganizationSwitcher } from '@/components/organization-switcher'
 import { ProfileButton } from '@/components/profile-button'
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability()
   return (
     <header className="mx-auto flex max-w-[1280px] items-center justify-between">
       <div className="flex items-center justify-center gap-3">
         <h1 className="text-2xl font-bold">Logo</h1>
         <Slash className="size-3 -rotate-[30deg] text-border" />
         <OrganizationSwitcher />
+        {permissions?.can('get', 'Project') && <p>Projects</p>}
       </div>
       <div className="flex items-center gap-4">
         <ProfileButton />
